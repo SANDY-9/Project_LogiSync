53e3fcd1477102fc75d1b15efb4060765f94472c
SANDY <nnal0256@naver.com>
2024-06-25 23:20:15 +0900

    SigupScreen - Check UI 구현

--- Android/feature/signup/src/main/java/com/feature/signup/SignupScreen.kt
+++ Android/feature/signup/src/main/java/com/feature/signup/SignupScreen.kt
@@ -1,15 +1,88 @@
 package com.feature.signup
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowLeft
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.feature.signup.components.Check
 
 @Composable
 fun SignupScreen(
     navController: NavController,
+    modifier: Modifier = Modifier,
 ) {
-    // TODO UI Rendering
+    Column(
+        modifier = modifier.fillMaxSize(),
+    ) {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(56.dp),
+        ) {
+            IconButton(
+                modifier = modifier
+                    .padding(start = 4.dp)
+                    .align(Alignment.CenterStart),
+                onClick = {
+                    navController.navigateUp()
+                },
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                    contentDescription = null,
+                )
+            }
+        }
+
+        Check()
+
+        Spacer(modifier = modifier.weight(1f))
+
+        Button(
+            modifier = modifier.fillMaxWidth()
+                .padding(16.dp),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(
+                text = stringResource(id = R.string.signup_next_step)
+            )
+        }
+    }
 }
 
 @Composable
--- Android/feature/signup/src/main/java/com/feature/signup/components/Check.kt
+++ Android/feature/signup/src/main/java/com/feature/signup/components/Check.kt
@@ -1,16 +1,73 @@
 package com.feature.signup.components
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.feature.signup.R
 
 // 회원가입 여부 확인
 @Composable
 fun Check(
     modifier: Modifier = Modifier
 ) {
-    Box(modifier) {
+    Column(
+        modifier = modifier.padding(16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.signup_check_title),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        
+        Spacer(modifier = modifier.height(16.dp))
+        
+        var name by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = {
+                Text(text = stringResource(id = R.string.signup_check_name_label))
+            },
+            placeholder = {
+                Text(text = stringResource(id = R.string.signup_check_name_placeholder))
+            },
+            singleLine = true,
+        )
+
+        var tel by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = tel,
+            onValueChange = { tel = it },
+            label = {
+                Text(text = stringResource(id = R.string.signup_check_tel_label))
+            },
+            placeholder = {
+                Text(text = stringResource(id = R.string.signup_check_tel_placeholder))
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal,
+                imeAction = ImeAction.Next,
+            ),
+            singleLine = true,
+        )
+
     }
 }
 
