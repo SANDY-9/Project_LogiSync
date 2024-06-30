d48875c4fd7c0ed71ee0aade14a7592ee92bd930
SANDY <nnal0256@naver.com>
2024-07-01 01:24:17 +0900

    core:domain 모듈 추가

---
+++ Android/core/domain/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.core.domain"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(project(":core:model"))
+    implementation(project(":core:data"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.coroutines.android)
+
+    // Inject
+    implementation(libs.inject)
+
+    testImplementation(libs.bundles.test)
+}
