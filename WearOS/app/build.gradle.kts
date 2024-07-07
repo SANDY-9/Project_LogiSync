a17fb13e1551168f1e8077bb390ef6173b22c9a9
SANDY <nnal0256@naver.com>
2024-07-07 21:35:19 +0900

    라이브러리 디펜던시 추가

--- WearOS/app/build.gradle.kts
+++ WearOS/app/build.gradle.kts
@@ -65,8 +65,12 @@ dependencies {
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
 
+    // Hilt
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
 }
 
