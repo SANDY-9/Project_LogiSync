ecb8f037f03adfcf04cf57d9edfaafad474a75d7
SANDY <nnal0256@naver.com>
2024-07-14 20:28:28 +0900

    feat: 최근 심박수 Prefs에 저장

--- WearOS/app/src/main/java/com/sandy/logisync/data/mapper/JsonMapper.kt
+++ WearOS/app/src/main/java/com/sandy/logisync/data/mapper/JsonMapper.kt
@@ -1,7 +1,9 @@
 package com.sandy.logisync.data.mapper
 
 import com.sandy.logisync.model.Account
+import com.sandy.logisync.model.HeartRate
 import org.json.JSONObject
+import java.time.LocalDateTime
 
 fun String.toAccount(): Account {
     val jsonObject = JSONObject(this)
@@ -10,5 +12,20 @@ fun String.toAccount(): Account {
         name = jsonObject.getString("name"),
         tel = jsonObject.getString("tel"),
         //duty = jsonObject.getString("duty")
+    )
+}
+
+fun HeartRate.toJson(): String {
+    val jsonObject = JSONObject()
+    jsonObject.put("date", date.toString())
+    jsonObject.put("value", value)
+    return jsonObject.toString()
+}
+
+fun String.toHeartRate(): HeartRate {
+    val jsonObject = JSONObject(this)
+    return HeartRate(
+        date = LocalDateTime.parse(jsonObject.getString("date")),
+        value = jsonObject.getInt("value")
     )
 }
