ca19f6195bb2cb486ba3e55a3a412213511775bc
SANDY <nnal0256@naver.com>
2024-07-14 20:29:06 +0900

    add: 심박수 수집시 최근 심박수 저장 로직 추가

--- WearOS/app/src/main/java/com/sandy/logisync/data/health/HealthMeasureRepositoryImpl.kt
+++ WearOS/app/src/main/java/com/sandy/logisync/data/health/HealthMeasureRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.sandy.logisync.data.health
 
+import com.sandy.logisync.data.datastore.WearableDataStoreRepository
 import com.sandy.logisync.data.mapper.toMeasuredHeartRate
 import com.sandy.logisync.model.MeasuredHeartRate
 import com.sandy.logisync.wearable.health.HeartRateServiceManager
@@ -9,10 +10,15 @@ import javax.inject.Inject
 
 class HealthMeasureRepositoryImpl @Inject constructor(
     private val heartRateServiceManager: HeartRateServiceManager,
+    private val wearableDataStoreRepository: WearableDataStoreRepository,
 ) : HealthMeasureRepository {
     override fun getMeasuredHeartRate(): Flow<MeasuredHeartRate> {
         return heartRateServiceManager.getHeartRateMeasureFlow().map {
-            it.toMeasuredHeartRate()
+            it.toMeasuredHeartRate().also { measuredHeartRate ->
+                measuredHeartRate.heartRate?.let { heartRate ->
+                    wearableDataStoreRepository.updateLastHeartRate(heartRate)
+                }
+            }
         }
     }
 }
