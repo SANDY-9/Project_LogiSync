c6487d7841a6316f71ec21a38830d6b4ec3aeafe
SANDY <nnal0256@naver.com>
2024-07-14 23:42:54 +0900

    style: 디버깅 완료 로그 삭제

--- WearOS/app/src/main/java/com/sandy/logisync/data/health/HealthMeasureRepositoryImpl.kt
+++ WearOS/app/src/main/java/com/sandy/logisync/data/health/HealthMeasureRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.sandy.logisync.data.health
 import com.sandy.logisync.data.datastore.WearableDataStoreRepository
 import com.sandy.logisync.data.mapper.toMeasuredHeartRate
 import com.sandy.logisync.model.MeasuredHeartRate
-import com.sandy.logisync.wearable.health.HeartRateServiceManager
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -12,6 +11,7 @@ class HealthMeasureRepositoryImpl @Inject constructor(
     private val heartRateServiceManager: HeartRateServiceManager,
     private val wearableDataStoreRepository: WearableDataStoreRepository,
 ) : HealthMeasureRepository {
+
     override fun getMeasuredHeartRate(): Flow<MeasuredHeartRate> {
         return heartRateServiceManager.getHeartRateMeasureFlow().map {
             it.toMeasuredHeartRate().also { measuredHeartRate ->
