184e92ae9fa9f85e72f15b39b38a2c205bd37f8b
SANDY <nnal0256@naver.com>
2024-07-15 12:47:02 +0900

    add: feature:statistics 모듈 추가

---
+++ WearOS/app/src/main/java/com/sandy/logisync/wearable/health/StartupReceiver.kt
@@ -0,0 +1,14 @@
+package com.sandy.logisync.wearable.health
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class StartupReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action != Intent.ACTION_BOOT_COMPLETED) return
+        context?.let {
+            HeartRateMonitoringWorker.registerWorker(it)
+        }
+    }
+}
