4edb67c1ca826a31da9a1f063a941ff2e096279a
SANDY <nnal0256@naver.com>
2024-07-20 12:07:24 +0900

    fix : 홈화면 - 프로필 부분 UI 개편

--- Android/core/designsystem/src/main/java/com/core/desinsystem/theme/Color.kt
+++ Android/core/designsystem/src/main/java/com/core/desinsystem/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val TransparentBlack = Color(0x42000000)
+val TransparentWhiteDeem = Color(0xb3ffffff)
+val TransparentWhite = Color(0x4dffffff)
+val LogiBlue = Color(0xFF006bc7)
+val LogiOrange = Color(0xFFf79200)
+val LogiRed = Color(0xFFe8141d)
+
+val HeartRed = Color(0xFFa7416c)
--- Android/feature/home/src/main/java/com/feature/home/components/Profile.kt
+++ Android/feature/home/src/main/java/com/feature/home/components/Profile.kt
@@ -1,16 +1,20 @@
 package com.feature.home.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,17 +23,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.core.desinsystem.common.BasicOutlinedButton
+import com.core.desinsystem.theme.TransparentBlack
 import com.core.model.Account
-import com.core.model.Member
 import com.feature.home.R
 
 @Composable
 fun Profile(
     dateStr: String,
-    member: Member,
+    account: Account,
     modifier: Modifier = Modifier,
 ) {
     Column(
@@ -37,19 +39,28 @@ fun Profile(
     ) {
         Text(
             text = dateStr,
-            style = MaterialTheme.typography.titleMedium
+            style = MaterialTheme.typography.titleMedium,
         )
         Spacer(modifier = modifier.height(12.dp))
         Box(
             modifier = modifier.fillMaxWidth()
         ) {
+            Text(
+                modifier = modifier.padding(start = 74.dp, top = 8.dp),
+                text = stringResource(id = R.string.home_profile_hello),
+                style = MaterialTheme.typography.labelSmall,
+            )
             Row(
-                modifier = modifier.height(70.dp),
+                modifier = modifier.height(60.dp),
             ) {
                 Image(
                     modifier = modifier
-                        .size(width = 60.dp, height = 70.dp)
-                        .clip(RoundedCornerShape(8.dp)),
+                        .size(width = 60.dp, height = 60.dp)
+                        .clip(CircleShape)
+                        .border(
+                            width = 1.dp,
+                            color = TransparentBlack,
+                        ),
                     contentScale = ContentScale.Crop,
                     painter = painterResource(id = com.core.desinsystem.R.drawable.and),
                     contentDescription = null
@@ -57,15 +68,15 @@ fun Profile(
                 Text(
                     modifier = modifier
                         .align(Alignment.Bottom)
-                        .padding(horizontal = 8.dp),
-                    text = member.name,
+                        .padding(horizontal = 12.dp),
+                    text = account.name,
                     style = MaterialTheme.typography.titleLarge,
                 )
                 Text(
-                    modifier = modifier
-                        .align(Alignment.Bottom),
-                    text = "${member.duty.str()}  |  ${member.team.name}",
-                    style = MaterialTheme.typography.titleSmall
+                    modifier = modifier.align(Alignment.Bottom)
+                        .padding(bottom = 2.dp),
+                    text = "${account.duty.str()}  |  ${account.team.name}",
+                    style = MaterialTheme.typography.titleSmall,
                 )
             }
             ProfileFixButton(
@@ -77,11 +88,16 @@ fun Profile(
 
 @Composable
 private fun ProfileFixButton(
+    onClick: () -> Unit = {},
     modifier: Modifier = Modifier,
 ) {
-    BasicOutlinedButton(
-        modifier = modifier,
-        title = stringResource(id = R.string.home_profile_fix),
-        onClick = { /*TODO*/ }
-    )
+    OutlinedButton(
+        modifier = modifier.defaultMinSize(minHeight = 1.dp, minWidth = 1.dp),
+        contentPadding = PaddingValues(vertical = 4.dp, horizontal = 16.dp),
+        onClick = onClick,
+    ) {
+        Text(
+            text = stringResource(id = R.string.home_profile_fix)
+        )
+    }
 }
