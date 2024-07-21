9c7977d7f2472d7e54a765f8fd3765e214794493
SANDY <nnal0256@naver.com>
2024-07-21 09:54:05 +0900

    fix : 오전/오후 잘못 출력 수정

--- WearOS/app/src/main/java/com/sandy/logisync/model/utils/DateUtil.kt
+++ WearOS/app/src/main/java/com/sandy/logisync/model/utils/DateUtil.kt
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter
 object DateUtil {
 
     private val dateFormatter = DateTimeFormatter.ofPattern("MM월 dd일")
-    private val timeFormatter = DateTimeFormatter.ofPattern("hh:mm")
+    private val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
 
     fun convertDate(date: LocalDateTime): String {
         val dateStr = date.format(dateFormatter)
