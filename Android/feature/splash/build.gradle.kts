8618da00013c1dc16dc01a4394927bd4502e20a7
SANDY <nnal0256@naver.com>
2024-08-06 21:06:19 +0900

    add: :feature:splash 모듈 추가

---
+++ Android/feature/splash/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.feature.splash"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+}
+
+dependencies {
+
+    implementation(project(":core:designsystem"))
+    implementation(project(":core:navigation"))
+    implementation(project(":core:domain"))
+    implementation(project(":core:data"))
+    implementation(project(":core:model"))
+
+    implementation(libs.bundles.androidx)
+    implementation(libs.coroutines.android)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.bundles.test)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // Compose
+    implementation(libs.bundles.compose)
+    implementation(platform(libs.androidx.compose.bom))
+
+}
---
+++ Android/feature/splash/src/main/java/com/feature/splash/SplashScreen.kt
@@ -0,0 +1,17 @@
+package com.feature.splash
+
+import android.window.SplashScreen
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+@Preview
+private fun SplashScreenPreview() {
+    SplashScreen()
+}
